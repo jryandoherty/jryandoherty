361c86383fb3b19aee76006593d46b065a1d69d1
Ryan Doherty <ryan@epicsea.com>
2020-08-14 13:07:38 -0400

    Portfolio
    Portfolio items

---
+++ portfolio/2013/Leads/assets/main.js
@@ -0,0 +1,22 @@
+
+/*
+     FILE ARCHIVED ON 12:21:15 Feb 23, 2014 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 00:47:26 Aug 08, 2019.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  LoadShardBlock: 62.325 (3)
+  esindex: 0.011
+  captures_list: 84.357
+  CDXLines.iter: 12.819 (3)
+  PetaboxLoader3.datanode: 226.871 (5)
+  exclusion.robots: 0.207
+  exclusion.robots.policy: 0.193
+  RedisCDXSource: 5.774
+  PetaboxLoader3.resolve: 81.988 (2)
+  load_resource: 300.324
+*/
